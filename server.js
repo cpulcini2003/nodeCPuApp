7a6df933d14610cda4730682d8c304c2faa9c71c
Carlo Pulcini <cpu@MBP-di-Alberto.homenet.telecomitalia.it>
2021-12-16 21:27:41 +0100

    message

--- server.js
+++ server.js
@@ -3,14 +3,15 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
+const PORT = 8082;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World My');
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
