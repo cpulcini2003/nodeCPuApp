97850a006c072bd3181f2e85397ad4a691834fef
cpulcini2003 <64283587+cpulcini2003@users.noreply.github.com>
2021-12-20 12:51:20 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -6,10 +6,22 @@ const express = require('express');
 const PORT = 8082;
 const HOST = '0.0.0.0';
 
+const HTMLPAGE = `<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Carlo's very basic web service</h1>
+
+<p>Testing Cloud Native tools</p>
+
+</body>
+</html>
+`
+
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send(HTMLPAGE);
 });
 
 app.listen(PORT, HOST);
